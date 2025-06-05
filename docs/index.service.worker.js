6db9efc612f780eff61416474e8c2dddd60c68a1
Carlo Salva <scjsalva@gmail.com>
2025-06-05 12:55:50 +0800

    Reexport all

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749097772|5771272946';
+const CACHE_VERSION = '1749099218|73055722';
 /** @type {string} */
 const CACHE_PREFIX = 'HBDCozy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
