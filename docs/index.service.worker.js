d9871bb19982b3e4d5e48c5596cdd5da7abb0745
Carlo Salva <scjsalva@gmail.com>
2025-06-07 12:23:30 +0800

    Fix sizing issue

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749270137|2166416012';
+const CACHE_VERSION = '1749270206|2236040774';
 /** @type {string} */
 const CACHE_PREFIX = 'HBDCozy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
