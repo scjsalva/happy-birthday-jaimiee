3c521abf0c4d2ae4b3ada20040b41c8873d67997
Carlo Salva <scjsalva@gmail.com>
2025-06-16 12:00:42 +0800

    Add dialogues

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749272333|4362379729';
+const CACHE_VERSION = '1750046379|1720716421';
 /** @type {string} */
 const CACHE_PREFIX = 'HBDCozy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
